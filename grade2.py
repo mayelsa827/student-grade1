a92a993035d4fe399387752002f8f767c7af8449
May Thu Myint <mayelsa827@gmail.com>
2026-09-15 13:09:17 +0630

    Create student grade python

---
+++ grade2.py
@@ -0,0 +1,10 @@
+name = input("Enter student name: ")
+mark = int(input("Enter mark: "))
+
+if mark >= 50:
+    result = "Pass"
+else:
+    result = "Fail"
+
+print("Student:", name)
+print("Result:", result)
