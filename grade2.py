515122b51e3b3b0b36414cca37a11be5cc794915
May Thu Myint <mayelsa827@gmail.com>
2026-09-15 13:16:49 +0630

    Add grade calculation

--- grade2.py
+++ grade2.py
@@ -7,4 +7,14 @@ else:
     result = "Fail"
 
 print("Student:", name)
+print("Result:", resultname = input("Enter student name: ")
+mark = int(input("Enter mark: "))
+
+if mark >= 50:
+    result = "Pass"
+else:
+    result = "Fail"
+
+print("Student:", name)
 print("Result:", result)
+)
